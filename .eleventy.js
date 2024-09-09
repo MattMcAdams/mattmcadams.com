409010b555bf455c8e52c1067ccd337a7a538c6e
Matthew McAdams <mattmcadams@outlook.com>
2024-09-09 18:06:20 -0500

    Reorganize config

--- .eleventy.js
+++ .eleventy.js
@@ -6,31 +6,64 @@ const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 
 module.exports = function (eleventyConfig) {
-  /* ==================================================================
-  Add Plugins
-  ================================================================== */
+  /* =================================================================
+  /* SECTION Configuration
+  ================================================================= */
+
+  // Add Plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  /* ==================================================================
-  Data Deep Merge
-  https://www.11ty.dev/docs/data-deep-merge/
-  ================================================================== */
+
+  // Data Deep Merge
+  // https://www.11ty.dev/docs/data-deep-merge/
   eleventyConfig.setDataDeepMerge(true);
-  /* ==================================================================
-  Date Filters
-  ================================================================== */
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
-      "dd LLLL yyyy"
-    );
+
+  // Enable Static Passthrough
+  eleventyConfig.addPassthroughCopy({ "src/_static/": "/" });
+
+  /* ==============================
+  /* SECTION Markdown
+  ============================== */
+
+  let markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: false,
+  }).use(markdownItAnchor, {
+    permalink: true,
+    permalinkClass: "direct-link",
+    permalinkSymbol: "&sect;",
   });
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
+  /* !SECTION Markdown */
+  /* ==============================
+  /* SECTION BrowserSync Overrides (used only with --serve)
+  ============================== */
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync("dist/404.html");
+
+        browserSync.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          res.write(content_404);
+          res.end();
+        });
+      },
+    },
+    ui: false,
+    ghostMode: false,
   });
-  /* ==================================================================
-  General Filters
-  ================================================================== */
+
+  /* !SECTION BrowserSync Overrides */
+  /* !SECTION Configuration */
+  /* =================================================================
+  /* SECTION Filters
+  ================================================================= */
+
   // Get the first `n` elements of a collection.
   // eleventyConfig.addFilter("head", (array, n) => {
   //   if (n < 0) {
@@ -42,19 +75,41 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("min", (...numbers) => {
     return Math.min.apply(null, numbers);
   });
-  /* ==================================================================
-  Tag filter
-  Filters out unwanted / utility tags
-  ================================================================== */
+
+  /* ==============================
+  /* SECTION Dates
+  ============================== */
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLLL yyyy"
+    );
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  /* !SECTION Dates */
+  /* ==============================
+  /* SECTION Tag Filters
+  ============================== */
+
+  // Filters out unwanted / utility tags
   function filterTagList(tags) {
     return (tags || []).filter(
       (tag) => ["posts", "projects"].indexOf(tag) === -1
     );
   }
   eleventyConfig.addFilter("filterTagList", filterTagList);
-  /* ==================================================================
-  Collection Functions
-  ================================================================== */
+
+  /* !SECTION Tag Filters */
+  /* !SECTION Filters */
+  /* =================================================================
+  /* SECTION Collection Functions
+  ================================================================= */
+
   // Return a list of tags in a given collection
   function getTagList(collection) {
     let tagSet = new Set();
@@ -63,6 +118,7 @@ module.exports = function (eleventyConfig) {
     });
     return filterTagList([...tagSet]);
   }
+
   // Return a list of years in a given collection
   function getYearList(collection) {
     let years = new Set();
@@ -73,12 +129,14 @@ module.exports = function (eleventyConfig) {
     });
     return [...years];
   }
+
   // Find collection item by URL
   // {%- set page1 = collections.projects | find("/slug/") -%}
   // <h2>{{ page1.data.title }}</h2>
   eleventyConfig.addFilter("find", function find(collection = [], slug = "") {
     return collection.find((post) => post.url === slug);
   });
+
   // Return an object with arrays of posts by tag from the provided collection
   function createCollectionsByTag(collection) {
     // set the result as an object
@@ -99,6 +157,7 @@ module.exports = function (eleventyConfig) {
     // { tag-name: [post-object, post-object], tag-name: [post-object, post-object] }
     return resultArrays;
   }
+
   // Return an object with arrays of posts by year from the provided collection
   function createCollectionsByYear(collection) {
     // set the result as an object
@@ -117,9 +176,15 @@ module.exports = function (eleventyConfig) {
     // { year: [post-object, post-object], year: [post-object, post-object] }
     return resultArrays;
   }
-  /* ==================================================================
-  Collections
-  ================================================================== */
+
+  /* !SECTION Collection Functions */
+  /* =================================================================
+  /* SECTION Collections
+  ================================================================= */
+  /* ==============================
+  /* SECTION Posts
+  ============================== */
+
   // set up collections for posts
   const POSTS = (collectionAPI) => {
     return collectionAPI.getFilteredByGlob("./src/posts/*/*.md");
@@ -128,13 +193,13 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("posts", function (collectionAPI) {
     return POSTS(collectionAPI);
   });
-  // collections.postsByTag[tag] => Returns list of all posts that match tag-name
-  eleventyConfig.addCollection("postsByTag", function (collectionAPI) {
-    return createCollectionsByTag(POSTS(collectionAPI));
-  });
   // collections.postTags => Returns list of all tags
   eleventyConfig.addCollection("postTags", function (collectionAPI) {
     return getTagList(POSTS(collectionAPI));
+  });
+  // collections.postsByTag[tag] => Returns list of all posts that match tag-name
+  eleventyConfig.addCollection("postsByTag", function (collectionAPI) {
+    return createCollectionsByTag(POSTS(collectionAPI));
   });
   // Create a collection for post years (required for the generation of archive pages)
   eleventyConfig.addCollection("postYears", function (collectionAPI) {
@@ -145,6 +210,11 @@ module.exports = function (eleventyConfig) {
     return createCollectionsByYear(POSTS(collectionAPI));
   });
 
+  /* !SECTION Posts */
+  /* ==============================
+  /* SECTION Projects
+  ============================== */
+
   // Set up collections for projects
   const PROJECTS = (collectionAPI) => {
     return collectionAPI.getFilteredByGlob("./src/projects/*/*.md");
@@ -153,14 +223,19 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("projects", function (collectionAPI) {
     return PROJECTS(collectionAPI);
   });
-  // collections.projectsByTag[tag] => Returns list of all projects that match tag-name
-  eleventyConfig.addCollection("projectsByTag", function (collectionAPI) {
-    return createCollectionsByTag(PROJECTS(collectionAPI));
-  });
   // collections.projectTags => Returns list of all tags
   eleventyConfig.addCollection("projectTags", function (collectionAPI) {
     return getTagList(PROJECTS(collectionAPI));
   });
+  // collections.projectsByTag[tag] => Returns list of all projects that match tag-name
+  eleventyConfig.addCollection("projectsByTag", function (collectionAPI) {
+    return createCollectionsByTag(PROJECTS(collectionAPI));
+  });
+
+  /* !SECTION Projects */
+  /* ==============================
+  /* SECTION Sketchbook
+  ============================== */
 
   // Setup collections for sketchbook
   const SKETCHBOOK = (collectionAPI) => {
@@ -170,13 +245,13 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("sketchbook", function (collectionAPI) {
     return SKETCHBOOK(collectionAPI);
   });
-  // collections.sketchbookByTag[tag] => Returns list of all sketches that match tag-name
-  eleventyConfig.addCollection("sketchbookByTag", function (collectionAPI) {
-    return createCollectionsByTag(SKETCHBOOK(collectionAPI));
-  });
   // collections.sketchbookTags => Returns list of all tags
   eleventyConfig.addCollection("sketchbookTags", function (collectionAPI) {
     return getTagList(SKETCHBOOK(collectionAPI));
+  });
+  // collections.sketchbookByTag[tag] => Returns list of all sketches that match tag-name
+  eleventyConfig.addCollection("sketchbookByTag", function (collectionAPI) {
+    return createCollectionsByTag(SKETCHBOOK(collectionAPI));
   });
   // Create a collection for sketchbook years (required for the generation of archive pages)
   eleventyConfig.addCollection("sketchbookYears", function (collectionAPI) {
@@ -186,6 +261,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("sketchbookByYear", function (collectionAPI) {
     return createCollectionsByYear(SKETCHBOOK(collectionAPI));
   });
+
+  /* !SECTION Sketchbook */
+  /* ==============================
+  /* SECTION Logs
+  ============================== */
 
   // Setup collection for logs
   const LOGS = (collectionAPI) => {
@@ -204,47 +284,19 @@ module.exports = function (eleventyConfig) {
     return createCollectionsByYear(LOGS(collectionAPI));
   });
 
+  /* !SECTION Logs */
+
   // collections.allContent => Returns list of all content
   eleventyConfig.addCollection("allContent", function (collectionAPI) {
-    return collectionAPI.getFilteredByGlob(["./src/sketchbook/*/*.md", "./src/posts/*/*.md", "./src/projects/*/*.md", "./src/log/*/*.md"]);
+    return collectionAPI.getFilteredByGlob([
+      "./src/sketchbook/*/*.md",
+      "./src/posts/*/*.md",
+      "./src/projects/*/*.md",
+      "./src/log/*/*.md",
+    ]);
   });
 
-  /* ==================================================================
-  Enable static passthrough
-  ================================================================== */
-  eleventyConfig.addPassthroughCopy({ "src/_static/": "/" });
-  /* ==================================================================
-  Customize Markdown library and settings
-  ================================================================== */
-  let markdownLibrary = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true,
-  }).use(markdownItAnchor, {
-    permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "&sect;",
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
-  /* ==================================================================
-  Override Browsersync defaults (used only with --serve)
-  ================================================================== */
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function (err, browserSync) {
-        const content_404 = fs.readFileSync("dist/404.html");
-
-        browserSync.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
-          res.write(content_404);
-          res.end();
-        });
-      },
-    },
-    ui: false,
-    ghostMode: false,
-  });
+  /* !SECTION Collections */
   /* =================================================================
   /* SECTION Shortcodes
   ================================================================= */
@@ -258,9 +310,10 @@ module.exports = function (eleventyConfig) {
   });
 
   /* !SECTION Shortcodes */
-  /* ==================================================================
-  RETURN
-  ================================================================== */
+  /* =================================================================
+  /* SECTION Return
+  ================================================================= */
+
   return {
     // Control which files Eleventy will process
     // e.g.: *.md, *.njk, *.html, *.liquid
@@ -283,4 +336,6 @@ module.exports = function (eleventyConfig) {
       output: "dist",
     },
   };
-};;
+
+  /* !SECTION Return */
+};
